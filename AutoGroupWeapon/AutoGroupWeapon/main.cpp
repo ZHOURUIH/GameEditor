1aa85176a2a6f328a83a41b78f5c765c45e6bf44
zhourui <785130190@qq.com>
2018-09-08 23:25:28 +0800

    添加人物和怪物的序列帧自动重命名

--- AutoGroupWeapon/AutoGroupWeapon/main.cpp
+++ AutoGroupWeapon/AutoGroupWeapon/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "Utility.h"
 
+#define ITOA(i) Utility::intToString(i)
+#define ATOI(s) Utility::stringToInt(s)
+
 std::string getSection(std::string& str, const std::string& key)
 {
 	if (str == "")
@@ -23,22 +26,77 @@ std::string getSection(std::string& str, const std::string& key)
 
 void main()
 {
-	std::cout << "输入武器ID:";
-	int weaponID = 0;
-	std::cin >> weaponID;
-	std::vector<std::string> fileList;
-	Utility::findFiles("./", fileList, ".png", false);
-	// 解析每个文件的信息
-	int fileCount = fileList.size();
-	for (int i = 0; i < fileCount; ++i)
+	std::cout << "武器(1)" << std::endl;
+	std::cout << "人物(2)" << std::endl;
+	std::cout << "怪物(3)" << std::endl;
+	int type = 0;
+	std::cin >> type;
+	if (type == 1)
 	{
-		std::string fileName = Utility::getFileName(fileList[i]);
-		int direction = Utility::stringToInt(getSection(fileName, "_"));
-		std::string action = getSection(fileName, "_");
-		int index = Utility::stringToInt(getSection(fileName, "_"));
-		// 拷贝到目标目录
-		std::string destFolder = "weapon_id" + Utility::intToString(weaponID) + "_dir" + Utility::intToString(direction) + "_" + action;
-		std::string destFileName = destFolder + "_" + Utility::intToString(index) + ".png";
-		Utility::copyFile(fileList[i], destFolder + "/" + destFileName);
+		std::cout << "输入武器ID:";
+		int weaponID = 0;
+		std::cin >> weaponID;
+		std::vector<std::string> fileList;
+		Utility::findFiles("./", fileList, ".png", false);
+		// 解析每个文件的信息
+		int fileCount = fileList.size();
+		for (int i = 0; i < fileCount; ++i)
+		{
+			std::string fileName = Utility::getFileName(fileList[i]);
+			int direction = ATOI(getSection(fileName, "_"));
+			std::string action = getSection(fileName, "_");
+			int index = ATOI(getSection(fileName, "_"));
+			// 拷贝到目标目录
+			std::string destFolder = "weapon_id" + ITOA(weaponID) + "_dir" + ITOA(direction) + "_" + action;
+			std::string destFileName = destFolder + "_" + ITOA(index) + ".png";
+			Utility::copyFile(fileList[i], destFolder + "/" + destFileName);
+		}
+	}
+	else if (type == 2)
+	{
+		std::cout << "输入职业ID:";
+		int occupation = 0;
+		std::cin >> occupation;
+		std::cout << "输入衣服ID:";
+		int humanID = 0;
+		std::cin >> humanID;
+
+		std::vector<std::string> fileList;
+		Utility::findFiles("./", fileList, ".png", false);
+		// 解析每个文件的信息
+		int fileCount = fileList.size();
+		for (int i = 0; i < fileCount; ++i)
+		{
+			std::string fileName = Utility::getFileName(fileList[i]);
+			int direction = ATOI(getSection(fileName, "_"));
+			std::string action = getSection(fileName, "_");
+			int index = ATOI(getSection(fileName, "_"));
+			// 拷贝到目标目录
+			std::string destFolder = "human_occup" + ITOA(occupation) + "_dir" + ITOA(direction) + "_cloth" + ITOA(humanID) + "_" + action;
+			std::string destFileName = destFolder + "_" + ITOA(index) + ".png";
+			Utility::copyFile(fileList[i], destFolder + "/" + destFileName);
+		}
+	}
+	else if (type == 3)
+	{
+		std::cout << "输入怪物ID:";
+		int monsterID = 0;
+		std::cin >> monsterID;
+
+		std::vector<std::string> fileList;
+		Utility::findFiles("./", fileList, ".png", false);
+		// 解析每个文件的信息
+		int fileCount = fileList.size();
+		for (int i = 0; i < fileCount; ++i)
+		{
+			std::string fileName = Utility::getFileName(fileList[i]);
+			int direction = ATOI(getSection(fileName, "_"));
+			std::string action = getSection(fileName, "_");
+			int index = ATOI(getSection(fileName, "_"));
+			// 拷贝到目标目录
+			std::string destFolder = "monster_id" + ITOA(monsterID) + "_dir" + ITOA(direction) + "_" + action;
+			std::string destFileName = destFolder + "_" + ITOA(index) + ".png";
+			Utility::copyFile(fileList[i], destFolder + "/" + destFileName);
+		}
 	}
 }
