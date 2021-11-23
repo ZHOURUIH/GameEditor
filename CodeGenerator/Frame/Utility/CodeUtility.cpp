3ed6926c6f70c1c1485ea964b20949b79f81ce5e
zhourui <785130190@qq.com>
2021-11-24 00:13:36 +0800

    更新网络消息代码生成

--- CodeGenerator/Frame/Utility/CodeUtility.cpp
+++ CodeGenerator/Frame/Utility/CodeUtility.cpp
@@ -235,26 +235,7 @@ string CodeUtility::nameToUpper(const string& sqliteName, bool preUnderLine)
 
 string CodeUtility::cppPushParamString(const PacketMember& memberInfo)
 {
-	string str;
-	if (memberInfo.mIsArray)
-	{
-		string lengthMacro;
-		uint macroCount = memberInfo.mArrayLengthMacro.size();
-		FOR_I(macroCount)
-		{
-			lengthMacro += "GD::" + memberInfo.mArrayLengthMacro[i];
-			if (i != macroCount - 1)
-			{
-				lengthMacro += " * ";
-			}
-		}
-		str = "pushParam<" + memberInfo.mTypeName + ", " + lengthMacro + ">(" + memberInfo.mMemberName + ");";
-	}
-	else
-	{
-		str = "pushParam(" + memberInfo.mMemberName + ");";
-	}
-	return str;
+	return "pushParam(" + memberInfo.mMemberName + ");";
 }
 
 string CodeUtility::cppMemberDeclareString(const PacketMember& memberInfo)
