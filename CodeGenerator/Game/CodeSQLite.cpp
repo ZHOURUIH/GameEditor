4a14c57573ad36522fe3b529a6dda42f19c24b55
zhourui <785130190@qq.com>
2021-12-05 00:14:00 +0800

    修正代码生成错误

--- CodeGenerator/Game/CodeSQLite.cpp
+++ CodeGenerator/Game/CodeSQLite.cpp
@@ -476,9 +476,9 @@ void CodeSQLite::generateCSharpSQLiteDataFile(const SQLiteInfo& sqliteInfo, stri
 		string typeName = sqliteInfo.mMemberList[i].mTypeName;
 		// 因为模板文件是按照C++来写的,但是有些类型在C#中是没有的,所以要转换为C#中对应的类型
 		// myVector替换为List,Vector2UShort替换为Vector2Int,char替换为sbyte
-		if (startWith(typeName, "Vector"))
+		if (startWith(typeName, "Vector<"))
 		{
-			strReplaceAll(typeName, "Vector", "List");
+			strReplaceAll(typeName, "Vector<", "List<");
 		}
 		else if (typeName == "Vector2UShort")
 		{
