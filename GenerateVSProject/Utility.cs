b8fe590320f6ced2056b6b281fd5e10fa084e656
rui.zhou <rui.zhou@appshahe.com>
2023-05-12 11:56:00 +0800

    整理代码

--- GenerateVSProject/Utility.cs
+++ GenerateVSProject/Utility.cs
@@ -1,182 +1,203 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Xml;
-using static Utility;
 
-class Program
+class Utility
 {
-	static void Main(string[] args)
+	// 目录是否存在,dir是绝对路径
+	public static bool isDirExist(string dir)
 	{
-		FileStream configFile = new FileStream("./GenerateVSProjectConfig.txt", FileMode.Open);
-		byte[] bytes = new byte[configFile.Length];
-		configFile.Read(bytes, 0, (int)configFile.Length);
-		string[] lines = removeAll(Encoding.UTF8.GetString(bytes), '\r').Split('\n');
-		Dictionary<string, string> paramMap = new Dictionary<string, string>();
-		foreach (string line in lines)
+		if (dir.Length == 0 || dir == "./" || dir == "../")
 		{
-			string[] paramPair = line.Split('=');
-			if (paramPair.Length != 2)
-			{
-				continue;
-			}
-			paramMap.Add(paramPair[0], paramPair[1]);
+			return true;
 		}
-		if (!paramMap.TryGetValue("ProjectFilePath", out string projectFilePath))
+		validPath(ref dir);
+		return Directory.Exists(dir);
+	}
+	public static void validPath(ref string path)
+	{
+		if (path.Length == 0)
 		{
 			return;
 		}
-		string[] projectFileList = projectFilePath.Split(',');
-		foreach (string file in projectFileList)
+		// 不以/结尾,则加上/
+		if (path[path.Length - 1] != '/')
 		{
-			generateVCXProject(file);
-			generateVCXProjectFilters(file + ".filters");
+			path += "/";
 		}
 	}
-	// projectPath需要是绝对路径
-	protected static void generateVCXProject(string fileName)
+	// 查找指定目录下的所有文件,path为绝对路径
+	public static void findFiles(string path, List<string> fileList, List<string> patterns = null, bool recursive = true)
 	{
-		if (!fileName.EndsWith(".vcxproj"))
+		validPath(ref path);
+		if (!isDirExist(path))
 		{
-			Console.WriteLine("项目文件名错误:" + fileName);
-			Console.Read();
 			return;
 		}
-
-		XmlDocument doc = openXML(fileName);
-		XmlNamespaceManager nsMgr = new XmlNamespaceManager(doc.NameTable);
-		nsMgr.AddNamespace("ns", "http://schemas.microsoft.com/developer/msbuild/2003");
-
-		XmlNode itemGroupNode = doc.SelectSingleNode("/ns:Project", nsMgr);
-		// 删除有ClInclude或者ClCompile的ItemGroup
-		DeleteChildWithInnerChild(itemGroupNode, "ItemGroup", new List<string>() { "ClInclude", "ClCompile" });
-
-		XmlNode newNode = doc.CreateElement("ItemGroup", doc.DocumentElement.NamespaceURI);
-		// 查找当前目录中的所有代码文件.h .cpp .c .inl
-		List<string> sourceFiles = new List<string>();
-		string projectPath = getFilePath(fileName) + "/";
-		findFiles(projectPath, sourceFiles, new List<string>() { ".h", ".cpp", ".c", ".inl" });
-		foreach (string source in sourceFiles)
+		DirectoryInfo folder = new DirectoryInfo(path);
+		FileInfo[] fileInfoList = folder.GetFiles();
+		int fileCount = fileInfoList.Length;
+		int patternCount = patterns != null ? patterns.Count : 0;
+		for (int i = 0; i < fileCount; ++i)
 		{
-			// 只编译.c和UnityBuild.cpp文件
-			string fileNameNoDir = getFileName(source);
-			if (fileNameNoDir.EndsWith(".c") || (fileNameNoDir.StartsWith("UnityBuild") && fileNameNoDir.EndsWith(".cpp")))
+			string fileName = fileInfoList[i].Name;
+			// 如果需要过滤后缀名,则判断后缀
+			if (patternCount > 0)
 			{
-				XmlElement newChild = doc.CreateElement("ClCompile", doc.DocumentElement.NamespaceURI);
-				newChild.SetAttribute("Include", source.Remove(0, projectPath.Length).Replace('/', '\\'));
-				newNode.AppendChild(newChild);
+				for (int j = 0; j < patternCount; ++j)
+				{
+					if (fileName.EndsWith(patterns[j], StringComparison.OrdinalIgnoreCase))
+					{
+						fileList.Add(path + fileName);
+					}
+				}
 			}
+			// 不需要过滤,则直接放入列表
 			else
 			{
-				XmlElement newChild = doc.CreateElement("ClInclude", doc.DocumentElement.NamespaceURI);
-				newChild.SetAttribute("Include", source.Remove(0, projectPath.Length).Replace('/', '\\'));
-				newNode.AppendChild(newChild);
-
-				// inl文件还有一个子节点
-				if (fileNameNoDir.EndsWith(".inl"))
-				{
-					XmlElement filterType = doc.CreateElement("FileType", doc.DocumentElement.NamespaceURI);
-					filterType.InnerText = "Document";
-					newChild.AppendChild(filterType);
-				}
+				fileList.Add(path + fileName);
 			}
 		}
-		if (newNode.ChildNodes.Count > 0)
+		// 查找所有子目录
+		if (recursive)
 		{
-			itemGroupNode.AppendChild(newNode);
+			string[] dirs = Directory.GetDirectories(path);
+			int count = dirs.Length;
+			for (int i = 0; i < count; ++i)
+			{
+				findFiles(dirs[i], fileList, patterns, recursive);
+			}
 		}
-
-		string newFileName = projectPath + "new_" + getFileName(fileName);
-		doc.Save(newFileName);
 	}
-	protected static void generateVCXProjectFilters(string fileName)
+	// 得到指定目录下的所有第一级子目录,path为绝对路径
+	public static bool findFolders(string path, List<string> dirList, List<string> excludeList = null, bool recursive = true)
 	{
-		XmlDocument doc = openXML(fileName);
-		XmlNamespaceManager nsMgr = new XmlNamespaceManager(doc.NameTable);
-		nsMgr.AddNamespace("ns", "http://schemas.microsoft.com/developer/msbuild/2003");
-
-		XmlNode itemGroupNode = doc.SelectSingleNode("/ns:Project", nsMgr);
-		// 删除有ClInclude或者Filter的ItemGroup
-		DeleteChildWithInnerChild(itemGroupNode, "ItemGroup", new List<string>() { "ClInclude", "Filter", "ClCompile" });
-
-		XmlElement filterItemGroup = doc.CreateElement("ItemGroup", doc.DocumentElement.NamespaceURI);
-		string projectPath = getFilePath(fileName) + "/";
-		List<string> sourceDirs = new List<string>();
-		findFolders(projectPath, sourceDirs);
-		foreach (string dir in sourceDirs)
+		validPath(ref path);
+		if (!isDirExist(path))
 		{
-			List<string> tempFileList = new List<string>();
-			findFiles(dir, tempFileList, new List<string>() { ".h", ".cpp", ".c", ".inl" });
-			if (tempFileList.Count == 0)
+			return false;
+		}
+		string[] dirs = Directory.GetDirectories(path);
+		int count = dirs.Length;
+		for (int i = 0; i < count; ++i)
+		{
+			string dir = dirs[i];
+			if (excludeList != null && excludeList.Count > 0)
+			{
+				if (excludeList.Contains(getFileName(dir)))
+				{
+					continue;
+				}
+			}
+			dirList.Add(dir);
+			if (recursive)
+			{
+				findFolders(dir, dirList, excludeList, recursive);
+			}
+		}
+		return true;
+	}
+	public static string removeAll(string str, char key)
+	{
+		StringBuilder builder = new StringBuilder(str);
+		for (int i = builder.Length - 1; i >= 0; --i)
+		{
+			// 判断是否是需要移除的字符
+			if (builder[i] == key)
+			{
+				builder.Remove(i, 1);
+			}
+		}
+		return builder.ToString();
+	}
+	public static int lastIndexOf(StringBuilder builder, char value, int startIndex = -1)
+	{
+		int length = builder.Length;
+		if (startIndex < 0 || startIndex > length - 1)
+		{
+			startIndex = length - 1;
+		}
+		for (int i = startIndex; i >= 0; --i)
+		{
+			if (builder[i] == value)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	// 得到文件路径
+	public static string getFilePath(string fileName, bool keepEndSlash = false)
+	{
+		StringBuilder builder = new StringBuilder(fileName);
+		builder.Replace('\\', '/');
+		// 从倒数第二个开始,因为即使最后一个是/也需要忽略
+		int lastPos = lastIndexOf(builder, '/', builder.Length - 2);
+		if (lastPos < 0)
+		{
+			return "";
+		}
+		if (keepEndSlash)
+		{
+			builder.Remove(lastPos + 1, builder.Length - lastPos - 1);
+		}
+		else
+		{
+			builder.Remove(lastPos, builder.Length - lastPos);
+		}
+		return builder.ToString();
+	}
+	public static string getFileName(string str)
+	{
+		StringBuilder builder = new StringBuilder(str);
+		builder.Replace('\\', '/');
+		int dotPos = lastIndexOf(builder, '/');
+		if (dotPos != -1)
+		{
+			builder.Remove(0, dotPos + 1);
+		}
+		return builder.ToString();
+	}
+	public static XmlDocument openXML(string fileName)
+	{
+		XmlDocument doc = new XmlDocument();
+		XmlReaderSettings settings = new XmlReaderSettings();
+		settings.IgnoreComments = true;
+		XmlReader reader = XmlReader.Create(fileName, settings);
+		doc.Load(reader);
+		reader.Close();
+		return doc;
+	}
+	// 从node中删除指定的deleteChildName,不过只删除有子节点的名字为childWithInnerChild的deleteChildName
+	public static void DeleteChildWithInnerChild(XmlNode node, string deleteChildName, List<string> childWithInnerChild)
+	{
+		// 删除有ClInclude或者ClCompile的ItemGroup
+		for (int i = 0; i < node.ChildNodes.Count; ++i)
+		{
+			XmlNode child = node.ChildNodes[i];
+			if (child.Name != deleteChildName)
 			{
 				continue;
 			}
-			XmlElement filterNode = doc.CreateElement("Filter", doc.DocumentElement.NamespaceURI);
-			filterNode.SetAttribute("Include", dir.Remove(0, projectPath.Length).Replace('/', '\\'));
-			XmlElement identifierNode = doc.CreateElement("UniqueIdentifier", doc.DocumentElement.NamespaceURI);
-			identifierNode.InnerText = Guid.NewGuid().ToString();
-			filterNode.AppendChild(identifierNode);
-			filterItemGroup.AppendChild(filterNode);
-		}
-		if (filterItemGroup.ChildNodes.Count > 0)
-		{
-			itemGroupNode.AppendChild(filterItemGroup);
-		}
-		
-		XmlElement includeItemGroup = doc.CreateElement("ItemGroup", doc.DocumentElement.NamespaceURI);
-		List<string> fileList = new List<string>();
-		findFiles(projectPath, fileList, new List<string>() { ".h", ".cpp", ".c", ".inl" });
-		foreach (string file in fileList)
-		{
-			// 只编译.c和UnityBuild.cpp文件,其他的都是include
-			string fileNameNoDir = getFileName(file);
-			if (!fileNameNoDir.EndsWith(".c") && (!fileNameNoDir.StartsWith("UnityBuild") || !fileNameNoDir.EndsWith(".cpp")))
+			bool needDelete = false;
+			int countInChild = child.ChildNodes.Count;
+			for (int j = 0; j < countInChild; ++j)
 			{
-				XmlElement includeNode = doc.CreateElement("ClInclude", doc.DocumentElement.NamespaceURI);
-				string relativeFileName = file.Remove(0, projectPath.Length).Replace('/', '\\');
-				includeNode.SetAttribute("Include", relativeFileName);
-				string filter = getFilePath(relativeFileName);
-				if (filter != null && filter.Length > 0)
+				XmlNode innerChild = child.ChildNodes[j];
+				if (childWithInnerChild.Contains(innerChild.Name))
 				{
-					XmlElement filterNode = doc.CreateElement("Filter", doc.DocumentElement.NamespaceURI);
-					filterNode.InnerText = filter.Replace('/', '\\');
-					includeNode.AppendChild(filterNode);
+					needDelete = true;
+					break;
 				}
-				includeItemGroup.AppendChild(includeNode);
+			}
+			if (needDelete)
+			{
+				node.RemoveChild(child);
+				--i;
 			}
 		}
-		if (includeItemGroup.ChildNodes.Count > 0)
-		{
-			itemGroupNode.AppendChild(includeItemGroup);
-		}
-
-		XmlElement compileItemGroup = doc.CreateElement("ItemGroup", doc.DocumentElement.NamespaceURI);
-		foreach (string file in fileList)
-		{
-			// 只编译.c和UnityBuild.cpp文件
-			string fileNameNoDir = getFileName(file);
-			if (fileNameNoDir.EndsWith(".c") || (fileNameNoDir.StartsWith("UnityBuild") && fileNameNoDir.EndsWith(".cpp")))
-			{
-				XmlElement conpileNode = doc.CreateElement("ClCompile", doc.DocumentElement.NamespaceURI);
-				string relativeFileName = file.Remove(0, projectPath.Length).Replace('/', '\\');
-				conpileNode.SetAttribute("Include", relativeFileName);
-				string filter = getFilePath(relativeFileName);
-				if (filter != null && filter.Length > 0)
-				{
-					XmlElement filterNode = doc.CreateElement("Filter", doc.DocumentElement.NamespaceURI);
-					filterNode.InnerText = filter.Replace('/', '\\');
-					conpileNode.AppendChild(filterNode);
-				}
-				compileItemGroup.AppendChild(conpileNode);
-			}
-		}
-		if (compileItemGroup.ChildNodes.Count > 0)
-		{
-			itemGroupNode.AppendChild(compileItemGroup);
-		}
-
-		string newFileName = projectPath + "new_" + getFileName(fileName);
-		doc.Save(newFileName);
 	}
 }
