0bf316a20d2dcf4d64d99d57700fc8ee2da177be
zhourui <785130190@qq.com>
2024-02-20 18:29:06 +0800

    添加生成C#项目文件的工程

---
+++ GenerateCSharpVSProject/Config.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using static Utility;
+
+public class Config
+{
+	public static Dictionary<string, Dictionary<string, string>> mParamMap = new Dictionary<string, Dictionary<string, string>>();
+	public static bool init(string path)
+	{
+		byte[] bytes = null;
+		try
+		{
+			using (FileStream configFile = new FileStream(path, FileMode.Open))
+			{
+				bytes = new byte[configFile.Length];
+				configFile.Read(bytes, 0, (int)configFile.Length);
+			}
+		}
+		catch
+		{
+			Console.WriteLine("找不到配置文件:" + path);
+			return false;
+		}
+		string rootPathKey = "RootPath";
+		string[] lines = removeAll(Encoding.UTF8.GetString(bytes), '\r').Split('\n');
+		if (!lines[0].StartsWith(rootPathKey + "="))
+		{
+			Console.WriteLine("第一行必须是" + rootPathKey);
+			return false;
+		}
+		string[] tempPair = lines[0].Split('=');
+		if (tempPair.Length != 2)
+		{
+			Console.WriteLine(rootPathKey + "配置错误");
+			return false;
+		}
+		string rootPath = tempPair[1];
+		lines[0] = null;
+
+		string curFlag = null;
+		foreach (string line in lines)
+		{
+			if (line == null || line.Length == 0)
+			{
+				continue;
+			}
+			if (line[0] == '[' && line[line.Length - 1] == ']')
+			{
+				curFlag = line;
+				mParamMap.Add(curFlag, new Dictionary<string, string>());
+				continue;
+			}
+			if (curFlag == null)
+			{
+				continue;
+			}
+			string[] paramPair = line.Split('=');
+			if (paramPair.Length != 2)
+			{
+				continue;
+			}
+			// 将RootPath替换为配置的值
+			mParamMap[curFlag].Add(paramPair[0], paramPair[1].Replace("{" + rootPathKey + "}", rootPath));
+		}
+		foreach (var item in mParamMap)
+		{
+			if (!item.Value.ContainsKey("ProjectFilePath"))
+			{
+				Console.WriteLine("配置文件错误");
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ GenerateCSharpVSProject/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using static Utility;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		if (!Config.init("./GenerateCSharpVSProject_Config.txt"))
+		{
+			Console.Read();
+			return;
+		}
+		
+		foreach (var item in Config.mParamMap)
+		{
+			if (!item.Value.TryGetValue("ProjectFilePath", out string projectFilePath))
+			{
+				return;
+			}
+			item.Value.TryGetValue("IncludePath", out string includePath);
+			generateVCXProject(projectFilePath, includePath?.Split(','));
+		}
+	}
+	// projectPath需要是绝对路径
+	protected static void generateVCXProject(string fileName, string[] includePathList)
+	{
+		if (!fileName.EndsWith(".csproj"))
+		{
+			Console.WriteLine("项目文件名错误:" + fileName);
+			Console.Read();
+			return;
+		}
+
+		XmlDocument doc = openXML(fileName);
+		XmlNamespaceManager nsMgr = new XmlNamespaceManager(doc.NameTable);
+		nsMgr.AddNamespace("ns", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+		XmlNode itemGroupNode = doc.SelectSingleNode("/ns:Project", nsMgr);
+		// 删除有ClInclude或者ClCompile的ItemGroup
+		DeleteChildWithInnerChild(itemGroupNode, "ItemGroup", new List<string>() { "Compile" });
+
+		XmlNode newNode = doc.CreateElement("ItemGroup", doc.DocumentElement.NamespaceURI);
+		// 查找当前目录中的所有代码文件.cs
+		List<string> sourceFiles = new List<string>();
+		string projectPath = getFilePath(fileName) + "/";
+		if (includePathList == null || includePathList.Length == 0)
+		{
+			findFiles(projectPath, sourceFiles, new List<string>() { ".cs" });
+		}
+		else
+		{
+			for (int i = 0; i < includePathList.Length; ++i)
+			{
+				findFiles(includePathList[i], sourceFiles, new List<string>() { ".cs" });
+			}
+		}
+		foreach (string source in sourceFiles)
+		{
+			XmlElement newChild = doc.CreateElement("Compile", doc.DocumentElement.NamespaceURI);
+			newChild.SetAttribute("Include", source.Remove(0, projectPath.Length).Replace('/', '\\'));
+			newNode.AppendChild(newChild);
+		}
+		if (newNode.ChildNodes.Count > 0)
+		{
+			itemGroupNode.AppendChild(newNode);
+		}
+		doc.Save(fileName);
+	}
+	protected static XmlNode findChild(XmlNode node, string name)
+	{
+		for (int i = 0; i < node.ChildNodes.Count; ++i)
+		{
+			XmlNode child = node.ChildNodes[i];
+			if (child.Name == name)
+			{
+				return child;
+			}
+		}
+		return null;
+	}
+}
